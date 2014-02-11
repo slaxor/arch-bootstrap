186af972e019c0f5017fb1adebaacea8f862a3b3
Sascha Teske <sascha.teske@gmail.com>
2014-02-11 07:39:00 +0100

    Add missing pacman deps

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -12,7 +12,7 @@
 # Some examples:
 #
 #   # arch-bootstrap destination
-#   # arch-bootstrap -a x86_64 -r ftp://ftp.archlinux.org destination-x86_64 
+#   # arch-bootstrap -a x86_64 -r ftp://ftp.archlinux.org destination-x86_64
 #
 # And then you can chroot to the destination directory (root/root):
 #
@@ -21,9 +21,11 @@
 set -e -u -o pipefail
 
 # Packages needed by pacman (see get-pacman-dependencies.sh)
+# Packages needed by pacman (see get-pacman-dependencies.sh)
 PACMAN_PACKAGES=(
   acl archlinux-keyring attr bzip2 curl expat glibc gpgme libarchive
   libassuan libgpg-error libssh2 lzo2 openssl pacman pacman-mirrorlist xz zlib
+  krb5 e2fsprogs keyutils
 )
 BASIC_PACKAGES=(${PACMAN_PACKAGES[*]} filesystem)
 EXTRA_PACKAGES=(coreutils bash grep gawk file tar systemd)
@@ -45,14 +47,14 @@ fetch() { wget -c --passive-ftp --quiet "$@"; }
 # Extract FILEPATH gz/xz archive to DEST directory
 uncompress() {
   local FILEPATH=$1 DEST=$2
-  
+
   case "$FILEPATH" in
     *.gz) tar xzf "$FILEPATH" -C "$DEST";;
     *.xz) xz -dc "$FILEPATH" | tar x -C "$DEST";;
     *) debug "Error: unknown package format: $FILEPATH"
        return 1;;
   esac
-}  
+}
 
 ###
 
@@ -65,25 +67,25 @@ configure_pacman() {
 
 configure_minimal_system() {
   local DEST=$1
-  
+
   mkdir -p "$DEST/dev"
-  echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd" 
+  echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd"
   echo 'root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi/:14657::::::' > "$DEST/etc/shadow"
   touch "$DEST/etc/group"
   echo "bootstrap" > "$DEST/etc/hostname"
-  
+
   test -e "$DEST/etc/mtab" || echo "rootfs / rootfs rw 0 0" > "$DEST/etc/mtab"
   test -e "$DEST/dev/null" || mknod "$DEST/dev/null" c 1 3
   test -e "$DEST/dev/random" || mknod -m 0644 "$DEST/dev/random" c 1 8
   test -e "$DEST/dev/urandom" || mknod -m 0644 "$DEST/dev/urandom" c 1 9
-  
+
   sed -i "s/^[[:space:]]*\(CheckSpace\)/# \1/" "$DEST/etc/pacman.conf"
   sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "$DEST/etc/pacman.conf"
 }
 
 fetch_packages_list() {
-  local REPO=$1 
-  
+  local REPO=$1
+
   debug "fetch packages list: $REPO/"
   # Force trailing '/' needed by FTP servers.
   fetch -O - "$REPO/" | extract_href | awk -F"/" '{print $NF}' | sort -rn ||
@@ -93,12 +95,12 @@ fetch_packages_list() {
 install_pacman_packages() {
   local BASIC_PACKAGES=$1 DEST=$2 LIST=$3 PACKDIR=$4
   debug "pacman package and dependencies: $BASIC_PACKAGES"
-  
+
   for PACKAGE in $BASIC_PACKAGES; do
     local FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\)$")
     test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; return 1; }
     local FILEPATH="$PACKDIR/$FILE"
-    
+
     debug "download package: $REPO/$FILE"
     fetch -O "$FILEPATH" "$REPO/$FILE"
     debug "uncompress package: $FILEPATH"
@@ -122,7 +124,7 @@ main() {
   test $# -eq 0 && set -- "-h"
   local ARCH=$DEFAULT_ARCH
   local REPO_URL=$DEFAULT_REPO_URL
-  
+
   while getopts "a:r:h" ARG; do
     case "$ARG" in
       a) ARCH=$OPTARG;;
@@ -132,7 +134,7 @@ main() {
   done
   shift $(($OPTIND-1))
   test $# -eq 1 || { show_usage; return 1; }
-  
+
   local DEST=$1
   local REPO="${REPO_URL%/}/core/os/$ARCH"
   local PACKDIR=$(mktemp -d)
@@ -140,7 +142,7 @@ main() {
   debug "destination directory: $DEST"
   debug "core repository: $REPO"
   debug "temporary directory: $PACKDIR"
-  
+
   # Fetch packages, install and do a minimal system configuration
   mkdir -p "$DEST"
   local LIST=$(fetch_packages_list $REPO)
@@ -150,7 +152,7 @@ main() {
   install_packages "$ARCH" "$DEST" "${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}"
   configure_pacman "$DEST" "$ARCH" # Pacman must be re-configured
   rm -rf "$PACKDIR"
-  
+
   debug "done"
 }
 
